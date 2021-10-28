0464bc8549ded537ee77fb4c947add7b6b343821
Cleanup Crew <92687025+Cleanup-Crew-From-Discord@users.noreply.github.com>
2021-10-28 12:16:26 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var floodedVally = extend(SectorPreset, "fuck", Planets.serpulo, 174, {
+    localizedName: "Forgotten Caldera",
+    description: "A forgotten research center, abandoned when the Crux invaded. Recover the technology before the Crux can. Gain the upper hand."
+    details: "Reports indicate experimental units still patrol this facility. They will be like nothing you have ever faced before. Be prepared."
+    difficulty: 10,
+    alwaysUnlocked: true
+});
